f79d8cad1bc5a1fc71982e6472c59cf820dde62b
Len <leonard.jagielski@capitalone.com>
2017-12-22 16:38:24 -0600

    Adds print statements for debugging

--- server.py
+++ server.py
@@ -155,4 +155,6 @@ if __name__ == '__main__':
             replace_existing=True
         )
     atexit.register(lambda: scheduler.shutdown())
+    print('Port = {}'.format(os.environ.get("PORT", 5000)))
+    print(type(os.environ.get("PORT", 5000)))
     app.run(debug=False, port=os.environ.get("PORT", 5000), host='0.0.0.0')
