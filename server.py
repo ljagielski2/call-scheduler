db1fc0691fe5d85e4b9df56c0cb3516ca66454c8
Len <leonard.jagielski@capitalone.com>
2017-12-22 16:45:12 -0600

    Casts port number to integer in app.run
    Heroku specifies a port as an environment variable, which was being returned as a string. It is now cast to an int

--- server.py
+++ server.py
@@ -155,6 +155,4 @@ if __name__ == '__main__':
             replace_existing=True
         )
     atexit.register(lambda: scheduler.shutdown())
-    print('Port = {}'.format(os.environ.get("PORT", 5000)))
-    print(type(os.environ.get("PORT", 5000)))
-    app.run(debug=False, port=os.environ.get("PORT", 5000), host='0.0.0.0')
+    app.run(debug=False, port=int(os.environ.get("PORT", 5000)), host='0.0.0.0')
