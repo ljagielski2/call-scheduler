d6c7414952f16d538346e5a4e7a7e828df7315c5
Len <leonard.jagielski@capitalone.com>
2018-01-02 13:40:36 -0600

    Updates deploy scripts

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -2,4 +2,6 @@
 wget -qO- https://toolbelt.heroku.com/install-ubuntu.sh | sh
 heroku plugins:install heroku-container-registry
 docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
-heroku container:push web --app $HEROKU_APP_NAME
+#heroku container:push web --app $HEROKU_APP_NAME
+docker tag ljagielski2/call-scheduler:latest registry.heroku.com/$HEROKU_APP_NAME/web
+docker push registry.heroku.com/$HEROKU_APP_NAME/web
