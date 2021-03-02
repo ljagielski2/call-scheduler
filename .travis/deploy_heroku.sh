886354734cbc412c6424ee78b2c1ec29efd8ffdf
Len <33946969+ljagielski2@users.noreply.github.com>
2021-03-02 08:41:19 -0600

    Add release to heroku deploy script

--- .travis/deploy_heroku.sh
+++ .travis/deploy_heroku.sh
@@ -4,3 +4,4 @@ docker login -u _ --password=$HEROKU_API_KEY registry.heroku.com
 #heroku container:push web --app $HEROKU_APP_NAME
 docker tag ljagielski2/call-scheduler:latest registry.heroku.com/$HEROKU_APP_NAME/web
 docker push registry.heroku.com/$HEROKU_APP_NAME/web
+heroku container:release web
